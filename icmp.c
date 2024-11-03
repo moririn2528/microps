258332f8bcfdd2bfce2f05e2cd1024f34a45e133
moririn2528 <strangenoise1@gmail.com>
2024-11-03 17:05:54 +0900

    step 11

--- icmp.c
+++ icmp.c
@@ -96,10 +96,38 @@ void icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst,
   debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)),
          ip_addr_ntop(dst, addr2, sizeof(addr2)), len);
   icmp_dump(data, len);
+
+  switch (hdr->type) {
+  case ICMP_TYPE_ECHO:
+    icmp_output(ICMP_TYPE_ECHOREPLY, hdr->code, hdr->values,
+                data + ICMP_HDR_SIZE, len - ICMP_HDR_SIZE, iface->unicast, src);
+    break;
+  default:
+    break;
+  }
 }
 
 int icmp_output(uint8_t type, uint8_t code, uint32_t values,
                 const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst) {
+  uint8_t buf[ICMP_BUFSIZ];
+  struct icmp_hdr *hdr;
+  size_t msg_len;
+  char addr1[IP_ADDR_STR_LEN];
+  char addr2[IP_ADDR_STR_LEN];
+
+  hdr = (struct icmp_hdr *)buf;
+  hdr->type = type;
+  hdr->code = code;
+  hdr->values = values;
+  hdr->sum = 0;
+  memcpy(hdr + 1, data, len);
+  msg_len = len + ICMP_HDR_SIZE;
+  hdr->sum = cksum16((uint16_t *)hdr, msg_len, 0);
+
+  debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)),
+         ip_addr_ntop(dst, addr2, sizeof(addr2)), msg_len);
+  icmp_dump(buf, msg_len);
+  return ip_output(IP_PROTOCOL_ICMP, buf, msg_len, src, dst);
 }
 
 int icmp_init(void) {
